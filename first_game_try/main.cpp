62ddfa98198f8c0ebdc00db4d5c30162d7454ba2
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-08-08 01:01:37 +0200

    Create main.cpp

---
+++ first_game_try/main.cpp
@@ -0,0 +1,35 @@
+
+#include "game_class.hpp"
+
+#define SCREEN_WIDTH 1280 
+#define SCREEN_HEIGHT 720
+const int FPS =60;
+const int FrameDelay = 1000/FPS;
+ Uint32  FrameStart ;
+ int FrameTime ; 
+Game *game = nullptr; 
+SDL_Renderer * Game:: renderer = nullptr;
+int main(int argc, char** argv){
+       game = new Game(); 
+        game -> init("game", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,500,500,0/*FULL SCREEN */ );
+        while (game -> running()){
+            //std :: cout << "works" ;
+            FrameStart =SDL_GetTicks(); 
+            game-> handle_eve();
+            game -> update();
+            game-> render();
+            FrameTime =SDL_GetTicks() - FrameStart ;  
+            if (FrameDelay>FrameTime)
+                SDL_Delay(FrameDelay-FrameTime);
+        }
+    game->clean();
+    
+}
+
+// g++   -lmingw32 -lSDL2main  -mwindows  -o main main.cpp -lSDL2 ; if ($?) { .\main }
+
+
+
+
+
+
